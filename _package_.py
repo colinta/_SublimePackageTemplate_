2f6c67c49ca19db66bc313eb91a629638ea39c52
Colin Thomas-Arnold <colinta@mac.com>
2012-01-13 10:35:43 -0700

    package template

---
+++ _package_.py
@@ -0,0 +1,20 @@
+import sublime
+import sublime_plugin
+
+
+class _PACKAGE_Command(sublime_plugin.TextCommand):
+    def run(self, edit, **kwargs):
+        e = self.view.begin_edit('_PACKAGE_')
+        regions = [region for region in self.view.sel()]
+
+        # sort by region.end() DESC
+        def compare(region_a, region_b):
+            return cmp(region_b.end(), region_a.end())
+        regions.sort(compare)
+
+        for region in regions:
+            self.run_each(edit, region, **kwargs)
+        self.view.end_edit(e)
+
+    def run_each(self, edit, region):
+        pass
